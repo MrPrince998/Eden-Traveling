f1cf03e3683e292581d1f71d116352e881824f91
MrPrince998 <peincerana234@gmail.com>
2025-04-15 07:58:41 +0545

    update

--- src/components/Adventure.jsx
+++ src/components/Adventure.jsx
@@ -10,10 +10,10 @@ const Adventure = () => {
         <p className='font-medium text-xl'>TOUR TYPES</p>
         <h2 className='font-semibold font-playfair text-[40px] text-white my-[10px]'>Adventure & Activity</h2>
         <p className='mb-[30px]'>Explore thrilling destinations that will get your heart racing. From rugged mountains to dense jungles, embark on unforgettable journeys that will test your limits and create lasting memories. Dive into the unknown, conquer nature's challenges, and write your own epic tale of adventure.</p>
-        <div className='md:flex md:flex-row md:justify-between md:items-left md:flex-wrap md:gap-5 md:w-full'>
+        <div className='md:flex md:flex-row md:justify-between md:items-left md:flex-wrap md:gap-4 md:w-full'>
             {
                 AdventureActivity.map((items) => (
-                <div key={items.id} className='py-5 px-[60px] flex flex-col items-center justify-center md:w-80 md:justify-self-start'>
+                <div key={items.id} className='py-5 px-[40px] flex flex-col items-center justify-center md:w-80 md:justify-self-start'>
                     <img src={items.image} alt={items.title} className='w-[85px] aspect-auto'/>
                     <h3 className='my-[11px] text-white font-bold text-3xl font-playfair'>{items.title}</h3>
                     <p className='font-normal text-lg text-center'>{items.description}</p>
